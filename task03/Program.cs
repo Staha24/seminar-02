a7d99885402aa009d791bc9cc98a8dba7e3ab996
stanislav <79920025475@yandex.ru>
2022-11-27 22:17:48 +0500

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,11 @@
+﻿// task12
+Console.Write("Введите первое число: ");
+int n =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m =Convert.ToInt32(Console.ReadLine());
+
+if (n % m == 0)
+    Console.Write ("Кратно");
+else
+    Console.Write ($"Не кратно, остаток {n % m}");
+    
