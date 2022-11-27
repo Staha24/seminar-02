16bda97bc1581e23df17b5d6b00f9eb7ad2f9b71
stanislav <79920025475@yandex.ru>
2022-11-27 21:26:26 +0500

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,10 @@
+﻿// task demo
+int n = new Random ().Next(10, 100); // [10, 99]
+int n1 = n / 10;
+int n2 = n % 10;
+Console.WriteLine ($"Случайное число: {n}");
+if (n1>n2)
+    Console.Write(n1);
+else
+    Console.Write(n2);
+
