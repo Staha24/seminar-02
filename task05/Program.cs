0e8c9c7251a2998173cc00b3947d7d68935c6cff
stanislav <79920025475@yandex.ru>
2022-11-27 23:18:20 +0500

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// task16
+Console.Write ("Введите 1-ое чмсло: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите 2-ое чмсло: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m)
+    Console.Write ("yes");
+else
+{
+    if (m * m == n)
+        Console.Write ("yes");
+    else 
+        Console.Write ("no");
+}
