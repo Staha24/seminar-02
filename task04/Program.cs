ab20919fda53431f97e51da22409fd3fb9dbb452
stanislav <79920025475@yandex.ru>
2022-11-27 22:46:07 +0500

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,13 @@
+﻿// task14
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 ==0)
+{
+    if (n % 23 ==0)
+        Console.Write("yes");
+    else
+        Console.Write("no");    
+}
+else
+    Console.Write("no");
+    
