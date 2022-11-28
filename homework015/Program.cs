cd521b470664fc0929817e02b22a971f0726fdd1
stanislav <79920025475@yandex.ru>
2022-11-28 19:37:10 +0500

    homework015

---
+++ homework015/Program.cs
@@ -0,0 +1,8 @@
+﻿// task15
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+    Console.Write("выходной");
+else
+    Console.Write("нет");
+    
