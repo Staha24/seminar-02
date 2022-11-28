f5bc093d63b0335dec01d14985251dc348030a60
stanislav <79920025475@yandex.ru>
2022-11-28 19:21:05 +0500

    homework013

---
+++ homework013/Program.cs
@@ -0,0 +1,10 @@
+﻿//task13
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ int n1 = n % 10;
+if ( n >=100)
+    Console.Write(n1);
+else
+    Console.Write("Третьей цифры нет");
+
+
